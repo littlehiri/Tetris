a6bd8b82ca8e437fd5df7081b4dd6a3d39ded7f6
littlehiri <alvarohiri@gmail.com>
2023-01-10 09:11:41 +0100

    mejora

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    //Ultima caida(bajando) de la pieza hace 0 segundos
+    //Última caída(bajada) de la pieza hace 0 segundos
     float lastFall = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,110 +20,145 @@ public class Piece : MonoBehaviour
         //Movimiento de la ficha a la izquierda
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            //Llamamos al metodo de movimiento horizontal y le pasamos la direccion izquierda
-            MovePieceHorizontal(-1);
+            //Llamamos al método de movimiento horizontal y le pasamos la dirección izquierda
+            MovePieceHorizontally(-1);
         }
         //Movimiento de la ficha a la derecha
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            //Llamamos al metodo de movimiento horizontal y le pasamos la direccion derecha
-            MovePieceHorizontal(1);
+            //Llamamos al método de movimiento horizontal y le pasamos la dirección derecha
+            MovePieceHorizontally(1);
         }
-        //Rotacion de la pieza
-        else if(Input.GetKeyDown(KeyCode.UpArrow))
+        //Rotación de la pieza
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            //roto la pieza hacia la derecha
-            this.transform.Rotate(0, 0, 90);
+            //Roto la pieza hacia la derecha
+            this.transform.Rotate(0, 0, -90);
 
-            //Si la posicion es valida
-            if (IsvalidPosition())
+            //Si la posición es válida
+            if (IsValidPiecePosition())
             {
-                //actualizamos la rejilla, guardando la nueva posicion en el Gridhelper
+                //Actualizamos la rejilla, guardando la nueva posición en el GridHelper
                 UpdateGrid();
             }
-            //Si la posicion no fuese valida
+            //Si la posición no fuese válida
             else
             {
-                //Revierto la rotacion hacia el lado contrario izquierdo
+                //Revierto la rotación hacia el lado contrario(izquierdo)
                 this.transform.Rotate(0, 0, 90);
             }
         }
-        //Mover la pieza hacia abajo al pulsar la tecla o cuando haya pasado
-
-    }
-    //Metodo para el movimiento horizontal
-    void MovePieceHorizontal(int direction) //con direction, le pasamos un numero para
-    {
-        //Muevo la pieza en la direccion dada
-        this.transform.position += new Vector3(direction, 0, 0);
-        //comprobamos si la nueva posicion es valida
-        if (IsvalidPosition())
+        //Mover la pieza hacia abajo al pulsar la tecla o cuando haya pasado más de un segundo desde la última vez que se movió
+        else if (Input.GetKeyDown(KeyCode.DownArrow) || (Time.time - lastFall) > 1.0f)
         {
-            //Actualizamos la rejill, guardando la nueva posición en el gridhelper
+            //Muevo la pieza hacia abajo una posición
+            this.transform.position += new Vector3(0, -1, 0);
+
+            // Si la posición es válida
+            if (IsValidPiecePosition())
+            {
+                //Actualizamos la rejilla, guardando la nueva posición en el GridHelper
+                UpdateGrid();
+            }
+            //Si la posición no fuese válida
+            else
+            {
+                //Revierto el movimiento hacia abajo sumando uno hacia arriba
+                this.transform.position += new Vector3(0, 1, 0);
+                //Si ya no pudiese bajar más, habría que detectar si es momento de borrar una fila
+                GridHelper.DeleteAllFullRows();
+                //Hacemos que aparezca una pieza nueva, llamando al PieceSpawner a su método
+                FindObjectOfType<PieceSpawner>().SpawnNextPiece();//Busca un objeto de ese tipo para poder usar sus métodos y variables
+                //Deshabilitamos este script para que esta pieza no vuelva a moverse
+                this.enabled = false;
+            }
+            //Reiniciamos el contador de tiempo
+            lastFall = Time.time;
+        }
+        //Cuento en milisegundos cuanto ha pasado desde la última caída
+        lastFall += Time.deltaTime;
+    }
+
+    //Método para el movimiento horizontal
+    void MovePieceHorizontally(int direction) //con direction, le pasamos un número para saber si el movimiento es a izquierda o a derecha
+    {
+        //Muevo la pieza en la dirección dada
+        this.transform.position += new Vector3(direction, 0, 0);
+        //Comprobamos si la nueva posición es válida
+        if (IsValidPiecePosition())
+        {
+            //Actualizamos la rejilla, guardando la nueva posición en el GridHelper
             UpdateGrid();
         }
+        //Si la posición no es válida
         else
         {
-            //Revertimos el movimiento a la posicion en la que estaba antes
+            //Revertimos el movimiento a la posición en la que estaba antes
             this.transform.position += new Vector3(-direction, 0, 0);
         }
     }
-    //Metodo que comprueba si la posicion en la que se encuentra ahora mismo la pieza, es o no valida
-    private bool IsvalidPosition()
+
+    //Método que comprueba si la posición en la que se encuentra ahora mismo la pieza, es o no válida
+    private bool IsValidPiecePosition()
     {
-        //Hacemos una pasada por todas las posiciones de los hijos de la pieza (los bloques)
+        //Hacemos una pasada por todas las posiciones de los hijos de la pieza(los bloques)
         foreach (Transform block in this.transform)
         {
-            //Recuperamos su posicion (la de los bloques, hijos de la pieza) y la redondeamos
+            //Recuperamos su posición (la de los bloques, hijos de la pieza) y la redondeamos para que no tenga decimales
             Vector2 pos = GridHelper.RoundVector(block.position);
 
-            //Si no esta dentro de los bordes, la posicion, la posicion no es valida. Es decir
-            if(!GridHelper.IsInsideBorder(pos))
+            //Si no está dentro de los bordes, la posición no es válida. Es decir, alguno de los bloques de la pieza se sale de los bordes o está encima de ellos
+            if (!GridHelper.IsInsideBorders(pos))
             {
-                //Si algun bloque de la pieza no está en una posicion valida
+                //Si algún bloque de la pieza no está en una posición válida
                 return false;
             }
 
-            //Si ya hay otro bloque en esa misma posicion, la posicion tampoco es valida.
-            //Como la posicion podria ser un float(tener decimales), la transformamos en numero
-            transform possibleObject = GridHelper.grid[(int)pos.x, (int)pos.y];
-            //Si ya hay una pieza y este no es hijo del mismo objeto (osea el bloque esta dentro de otro)
+            //Si ya hay otro bloque en esa misma posición, la posición tampoco es válida. 
+            //Como la posición podría ser un float(tener decimales), la transformamos en número entero
+            Transform possibleObject = GridHelper.grid[(int)pos.x, (int)pos.y];
+            //Si ya hay otro objeto y este no es hijo del mismo objeto (osea el bloque que hay es de otra pieza)
             if (possibleObject != null && possibleObject.parent != this.transform)
             {
-                //la posicion no es valida
+                //La posición no será valida
                 return false;
             }
-            //Si ninguna de las cosas anteriormente mencionadas se cumple, sera que este bloque
-            return true;
         }
+        //Si ninguna de las cosas anteriormente mencionadas se cumple, será que este bloque o pieza está en una posición válida
+        return true;
     }
+
+    //Método que actualiza la rejilla virtual tras mover las piezas o bloques a su nueva posición
+    //Lo haremos primero haciendo un borrado de bloques, poniendo primero todo a null, y luego poniendo las posiciones nuevas de esos bloques
     private void UpdateGrid()
     {
         //Comparamos si el padre del objeto coincide con el del bloque estamos mirando
         for (int y = 0; y < GridHelper.h; y++)
         {
-            //Despues por colummnas de cada fila
+            //Después por columnas de cada fila
             for (int x = 0; x < GridHelper.w; x++)
             {
-                //comprobamos si en esa posicion hay un bloque
-                if(GridHelper.grid[x, y] != null)
+                //Comprobamos si en esa posición no hay un bloque
+                if (GridHelper.grid[x, y] != null)
                 {
                     //Comprobamos si el padre del bloque es la pieza donde está este script metido
                     if (GridHelper.grid[x, y].parent == this.transform)
                     {
-                        //Se carga los bloques que quedan de esa pieza y pone esas posiciones
+                        //Se carga los bloques que quedan de esa pieza y pone esas posiciones a null
                         GridHelper.grid[x, y] = null;
                     }
                 }
             }
+            //Insertamos los bloques en las posiciones que deben estar
+            //Hacemos una pasada por cada uno de los bloques de la pieza actual
+            foreach (Transform block in this.transform)
+            {
+                //Cojo la posición donde esté cada uno de los hijos y la redondeo
+                Vector2 pos = GridHelper.RoundVector(block.position);
+                //Metemos esa posición en la posición de la rejilla virtual que le toque
+                GridHelper.grid[(int)pos.x, (int)pos.y] = block;
+            }
         }
-        //Insertamos los bloques en las posiciones que deben estar
-        //hacemos una pasada por cada uno de los bloques de la pieza actual
-        foreach (Transform transform in this.transform)
-        {
-            //cojo la posicion donde este cada uno de los hijos y la redondeo
-            Vector2 pos = GridHelper.RoundVector(block.position);
-            //Metemos
-        }
+        
     }
 }
--- Assets/Scripts/PieceSpawner.cs
+++ Assets/Scripts/PieceSpawner.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class PieceSpawner : MonoBehaviour
 {
-    //Array donde guardamos los objetos que seran spawneados
+    //Array donde guardamos los objetos que serán spawneados
     public GameObject[] levelPieces;
+
+    //Variables para saber la pieza que tenemos y la siguiente que nos dará el juego
+    public GameObject currentPiece, nextPiece;
+
     // Start is called before the first frame update
     void Start()
     {
-        //sacará una pieza al empezar el juego
+        //Sacamos la siguiente pieza para tenerla visible
+        nextPiece = Instantiate(levelPieces[0], this.transform.position, Quaternion.identity);
+        //Sacará una pieza al azar al empezar el juego
         SpawnNextPiece();
     }
 
@@ -19,15 +25,52 @@ public class PieceSpawner : MonoBehaviour
         
     }
 
-    //Metodo de spwanear cada pieza
+    //Método para spawnear piezas
     public void SpawnNextPiece()
     {
-        //Tomamos un valor aleatorio comprendido en la longitud del array
-        int i = Random.Range(0, levelPieces.Length); //random.range(valor más bajo, valor mas alto)
+        //Al ir a spawnear la siguiente la que teníamos guardada pasa a ser la actual, por lo que activamos su script para poder moverla
+        //Iniciamos la pieza actual a la siguiente
+        currentPiece = nextPiece;
+        //Activo esa pieza (su script para que esta funcione)
+        currentPiece.GetComponent<Piece>().enabled = true;
 
-        //instanciamos la pieza seleccionada en la posicion del objeto spawneado
-        Instantiate(levelPieces[i], this.transform.position, Quaternion.identity);
-        
-        //Quaternion.iden guarda la rotacion del objeto
+        //Para cada bloque dentro de esa pieza
+        foreach (SpriteRenderer child in this.gameObject.GetComponentsInChildren<SpriteRenderer>())
+        {
+            //Cogemos el color actual de ese bloque
+            Color currentColor = child.color;
+            //Hacemos algo transparente a ese bloque
+            currentColor.a = 1.0f; //La transparencia va entre 0 y 1
+            child.color = currentColor;
+        }
+
+        //Lamamos a la corutina que prepara la siguiente pieza
+        StartCoroutine("PrepareNextPiece");
+    }
+
+    //Corrutina para preparar la siguiente pieza
+    IEnumerator PrepareNextPiece()
+    {
+        //Esperamos antes de nada 0.1 segundos
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        //Tomamos un valor aleatorio comprendido en la longitud de todo el array
+        int i = Random.Range(0, levelPieces.Length); //Random.Range(valor más bajo, valor más alto)
+
+        //Instanciamos la pieza seleccionada en la posición del objeto spawneador
+        nextPiece = Instantiate(levelPieces[i], this.transform.position, Quaternion.identity); //Quaternion.identity es la rotación que tuviera en ese momento el objeto spawneador
+        //Instantiate(objeto a instanciar, posición en la que se instancia, rotación con la que se instancia)
+        //Desactivamos para que la siguiente pieza no se mueva su script
+        nextPiece.GetComponent<Piece>().enabled = false;
+        //Para cada bloque dentro de esa pieza
+        foreach (SpriteRenderer child in this.gameObject.GetComponentsInChildren<SpriteRenderer>())
+        {
+            //Cogemos el color actual de ese bloque
+            Color currentColor = child.color;
+            //Hacemos algo transparente a ese bloque
+            currentColor.a = 0.3f; //La transparencia va entre 0 y 1
+            child.color = currentColor;
+        }
+
     }
 }
